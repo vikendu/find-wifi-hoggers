99e322bf29afac217354676368dede26e214bfb9
Vikendu Singh <vikendu@gmail.com>
2018-11-29 22:20:26 +0530

    Now works on kernels that don't support ARQ scan
    Works on android terminal as well

--- users.py
+++ users.py
@@ -4,13 +4,14 @@ import subprocess
 import ipaddress
 from subprocess import Popen, PIPE
 import re
-import arpreq
+#import arpreq
 import urllib.request as urllib2
 import json
 import codecs
 
-url = 'https://macvendors.co/api/'
+url = 'http://macvendors.co/api/'
 network = ipaddress.ip_network('192.168.1.0/24')
+print('Checking first 20 IPs in your Subnet\n')
 for i in network.hosts():
 	i = str(i)
 	toping = Popen(['ping', '-c','3',i], stdout = PIPE)
@@ -18,28 +19,35 @@ for i in network.hosts():
 	hostalive = toping.returncode
 	if hostalive == 0:
 		print(i, 'Available')
-		pid = Popen(["arp", "-n", i], stdout = PIPE)
+
+		if(i == '192.168.1.1'):
+			print('Your Network Administrator')
+
+		#pid = Popen(["arp", "-n", i], stdout = PIPE)
+
+		pid = Popen(["ip", "neigh", "show", i], stdout = PIPE)
 		s = pid.communicate()[0].decode("utf-8")
 
 		mac = re.search(r"(([a-f\d]{1,2}\:){5}[a-f\d]{1,2})", s)
-		#Some Xiaomi devices return None
+		
 		if mac is None:
 
-			#arpreq for Xiaomi devices
-			mac = arpreq.arpreq(i)
-			print(mac)
+			print("Your Device\n\n")
 
-			request = urllib2.Request(url+mac, headers={'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'})
-			response = urllib2.urlopen(request)
-			reader = codecs.getreader("utf-8")
-			obj = json.load(reader(response))
-			print (obj['result']['company']+"\n\n");
+			#Uncomment the following to get your Device MAC and Manufacturer
+			#Only works on kernels with ARQ support
+
+			#request = urllib2.Request(url+mac, headers={'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'})
+			#response = urllib2.urlopen(request)
+			#reader = codecs.getreader("utf-8")
+			#obj = json.load(reader(response))
+			#print (obj['result']['company']+"\n\n");
 
 			continue
 
 		else:
 			mac = mac.groups()[0]
-			print(mac)
+			print('Physical ID: ',mac)
 			request = urllib2.Request(url+mac, headers={'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'})
 			response = urllib2.urlopen(request)
 			reader = codecs.getreader("utf-8")
