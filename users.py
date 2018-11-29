ff987d870576d247fc0dc079f4c98a412381629f
Vikendu Singh <vikendu@gmail.com>
2018-11-29 19:49:41 +0530

    Add files via upload
    added ability to show mac id of connected user

--- users.py
+++ users.py
@@ -1,6 +1,7 @@
 import subprocess
 import ipaddress
 from subprocess import Popen, PIPE
+import re
 
 network = ipaddress.ip_network('192.168.1.0/24')
 for i in network.hosts():
@@ -10,7 +11,15 @@ for i in network.hosts():
 	hostalive = toping.returncode
 	if hostalive == 0:
 		print(i, 'Available')
+		pid = Popen(["arp", "-n", i], stdout = PIPE)
+		s = pid.communicate()[0].decode("utf-8")
+		mac = re.search(r"(([a-f\d]{1,2}\:){5}[a-f\d]{1,2})", s)
+		if mac is None:
+			continue
 
+		else:
+			print(mac.groups()[0])
+	
 	elif(i == '192.168.1.20'):
 		break
 
