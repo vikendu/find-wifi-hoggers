21754375c685115be3192a81cabaac2531c73caa
Vikendu Singh <vikendu@gmail.com>
2018-11-30 21:10:35 +0530

    tweaks

--- users.py
+++ users.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-
-import subprocess
 import ipaddress
 from subprocess import Popen, PIPE
 import re
@@ -11,15 +9,21 @@ import codecs
 
 url = 'http://macvendors.co/api/'
 network = ipaddress.ip_network('192.168.1.0/24')
+
 print('©2018 Vikendu Singh')
 print('Version 1.7_rc9\nFeel free to share with others\n')
 print('Checking first 20 IPs in your Subnet...  255.255.255.0 ... \n')
+
 for i in network.hosts():
+
 	i = str(i)
-	toping = Popen(['ping', '-c','3',i], stdout = PIPE)
+
+	toping = Popen(['ping', '-c','1',i], stdout = PIPE)
 	output = toping.communicate()[0]
 	hostalive = toping.returncode
+
 	if hostalive == 0:
+
 		print(i, 'Available')
 
 		if(i == '192.168.1.1'):
@@ -37,23 +41,27 @@ for i in network.hosts():
 			print("Your Device\n\n")
 
 			#Uncomment the following to get your Device MAC and Manufacturer
-			#Only works on kernels with ARQ support
+			#Only works on kernels with ARP support(also uncomment the import)
 
 			#request = urllib2.Request(url+mac, headers={'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'})
 			#response = urllib2.urlopen(request)
 			#reader = codecs.getreader("utf-8")
 			#obj = json.load(reader(response))
+			
 			#print (obj['result']['company']+"\n\n");
 
 			continue
 
 		else:
 			mac = mac.groups()[0]
+
 			print('Physical ID: ',mac)
+
 			request = urllib2.Request(url+mac, headers={'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'})
 			response = urllib2.urlopen(request)
 			reader = codecs.getreader("utf-8")
 			obj = json.load(reader(response))
+
 			print (obj['result']['company']+"\n\n");
 
 	
@@ -62,5 +70,3 @@ for i in network.hosts():
 
 	else:
 		print(i, 'Unavailable\n\n')
-
-	
